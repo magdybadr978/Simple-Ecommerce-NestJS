ac71a8bc2ab007888a173e5ab653e0614671831c
magdybadr978 <magdibadr2002@gmail.com>
2024-09-30 16:12:57 +0300

    add vendor schema

--- src/models/vendor/vendor.schema.ts
+++ src/models/vendor/vendor.schema.ts
@@ -1,12 +1,14 @@
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
-import mongoose from "mongoose";
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
 
-@Schema({timestamps : true , discriminatorKey : "role"})
+export type VendorDocument = Vendor & Document;
+
+@Schema({ timestamps: true, discriminatorKey: 'role' })
 export class Vendor {
-  name : string;
-  phone : string;
-  password : string
-  readonly _id? : mongoose.Schema.Types.ObjectId;
+  name: string;
+  phone: string;
+  password: string;
+  readonly _id?: mongoose.Schema.Types.ObjectId;
 }
 
 export const vendorSchema = SchemaFactory.createForClass(Vendor);
