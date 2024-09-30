9c52fb1182940a1b9a2eb80b6f3784ad5175a8c2
magdybadr978 <magdibadr2002@gmail.com>
2024-09-30 19:51:07 +0300

    add imports on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { VendorDashboardModule } from './vendorDashboard/vendorDashboard.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/dinamoDB'),
+    VendorDashboardModule,
+  ],
   controllers: [],
   providers: [],
 })
