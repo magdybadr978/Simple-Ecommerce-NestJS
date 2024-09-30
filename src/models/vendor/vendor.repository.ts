d08a354868fd96f072b108e34c09b09a393b39f4
magdybadr978 <magdibadr2002@gmail.com>
2024-09-30 16:13:22 +0300

    add vendor repository

--- src/models/vendor/vendor.repository.ts
+++ src/models/vendor/vendor.repository.ts
@@ -1,11 +1,12 @@
-import { Document, Model } from "mongoose";
-import { AbstractRepository } from "../abstract.repository";
-import { Vendor } from "./vendor.schema";
-import { InjectModel } from "@nestjs/mongoose";
+import { Document, Model } from 'mongoose';
+import { AbstractRepository } from '../abstract.repository';
+import { Vendor } from './vendor.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
-
-export class VendorRepository extends AbstractRepository<Vendor>{
-  constructor(@InjectModel(Vendor.name) private vendorModel : Model<Vendor & Document>) {
+export class VendorRepository extends AbstractRepository<Vendor> {
+  constructor(
+    @InjectModel(Vendor.name) private vendorModel: Model<Vendor & Document>
+  ) {
     super(vendorModel);
   }
 }
