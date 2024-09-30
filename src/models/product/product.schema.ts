72eee9585ed48d9390ee7d75815183df2d7ef1d1
magdybadr978 <magdibadr2002@gmail.com>
2024-09-30 16:11:36 +0300

    add product schema

---
+++ src/models/product/product.schema.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document, Types } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ timestamps: true })
+export class Product {
+  @Prop({ type: String })
+  name: string;
+
+  @Prop({ type: String })
+  description: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Vendor' })
+  vendorId: Types.ObjectId;
+
+  readonly _id: mongoose.Schema.Types.ObjectId;
+}
+
+export const productSchema = SchemaFactory.createForClass(Product);
