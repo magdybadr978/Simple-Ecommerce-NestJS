d1de4060c8e3e7efe20683467ce1f92ce517cbc2
magdybadr978 <magdibadr2002@gmail.com>
2024-10-01 01:21:54 +0300

    add user service

---
+++ src/userDashboard/user/user.service.ts
@@ -0,0 +1,82 @@
+
+
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { Types } from "mongoose";
+import { UserRepository } from "src/models/user/user.repository";
+import bcrypt from "bcryptjs"
+@Injectable()
+export class UserService {
+  constructor(private readonly userRepository : UserRepository) {}
+
+
+    // signUp for user
+    async signUp(createUserDTO : {name : string , phone : string , password : string}){
+      // check if user exist
+      const userExist = await this.userRepository.getOne({ phone : createUserDTO.phone})
+      // if exist throw error
+      if(userExist) throw new BadRequestException("user already exist")
+      // Hash password
+      const hashPassword = await bcrypt.hash(createUserDTO.password,10)
+      // send response
+      return this.userRepository.create({...createUserDTO , password : hashPassword})
+    }
+     // signIn for user
+    async signIn(signInDTO : {phone : string , password : string}){
+      // check if user exist
+      const user = await this.userRepository.getOne({ phone : signInDTO.phone});
+      // if user not found
+      if (!user) throw new NotFoundException('User not found');
+       // Verify the password
+      const passwordValid = await bcrypt.compare(signInDTO.password, user.password);
+      // check valid password 
+      if (!passwordValid) throw new UnauthorizedException ('Invalid credentials');
+      // return user details 
+      return { user };
+    }
+
+    // Create a new User
+    async createUser(createUserDTO: {
+      name: string,
+      phone: string,
+      password: string,
+    }){
+      return this.userRepository.create(createUserDTO);
+    }
+  
+    // Get all Users
+    async getAllUsers() {
+      return await this.userRepository.getAll({});
+    }
+  
+    // Get a User by id
+    async getUserById(id: string){
+      // find User 
+      const user = await this.userRepository.getOne({ _id: new Types.ObjectId(id) });
+      // check id User exist
+      if(!user) throw new NotFoundException("Not found User");
+      // return response
+      return {user}
+    }
+  
+    // Update User data by Id
+    async updateUser(
+      id : string,
+      updateUserDTO : {name: string,phone: string,password: string}
+    ) {
+      return this.userRepository.update(
+        { _id: new Types.ObjectId(id) },
+        updateUserDTO,
+        { new: true },
+      );
+    }
+  
+    // Delete User data by Id
+    async deleteUser(id: string): Promise<{message : string}> {
+      // use method delete 
+      const user = await this.userRepository.delete({ _id: new Types.ObjectId(id) });
+      // check if user deleted or not
+      if(user.deletedCount == 0) throw new NotFoundException("vendor not found")
+      // send response
+      return {message : "Deleted vendor successfully"}
+    }
+}
