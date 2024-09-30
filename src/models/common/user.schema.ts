80ce5d7836b218164c6eb6fd3b570ff1c13f2e6c
magdybadr978 <magdibadr2002@gmail.com>
2024-09-30 16:12:36 +0300

    add user repository

--- src/models/common/user.schema.ts
+++ src/models/common/user.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 
-@Schema({ timestamps: true, discriminatorKey: 'type' })
+@Schema({ timestamps: true, discriminatorKey: 'role' })
 export class User {
   @Prop({ type: String })
   name: string;
@@ -12,6 +12,6 @@ export class User {
   @Prop({ type: String })
   password: string;
 
-  readonly _id:mongoose.Schema.Types.ObjectId;
+  readonly _id: mongoose.Schema.Types.ObjectId;
 }
-export const userSchema= SchemaFactory.createForClass(User)
+export const userSchema = SchemaFactory.createForClass(User);
