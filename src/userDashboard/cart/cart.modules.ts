90be5e3ee14cb1e0825085751247e372dbb7cd16
magdybadr978 <magdibadr2002@gmail.com>
2024-10-01 02:28:08 +0300

    fix: add providers and exports

--- src/userDashboard/cart/cart.modules.ts
+++ src/userDashboard/cart/cart.modules.ts
@@ -3,12 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Cart, cartSchema } from 'src/models/cart/cart.schema';
 import { CartController } from './cart.controller';
 import { CartRepository } from 'src/models/cart/cart.repository';
+import { CartService } from './cart.service';
 
 
 @Module({
   imports: [ MongooseModule.forFeature([{ name : Cart.name , schema : cartSchema}])],
   controllers: [CartModule],
-  providers: [CartController,CartRepository],
-  exports : [CartController,CartRepository]
+  providers: [CartService,CartRepository],
+  exports : [CartService,CartRepository]
 })
 export class CartModule {}
