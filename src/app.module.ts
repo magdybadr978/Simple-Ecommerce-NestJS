ac1973ba9aa9902545c075d2497934733ea9c14a
magdybadr978 <magdibadr2002@gmail.com>
2024-10-01 01:54:54 +0300

    fix: app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VendorDashboardModule } from './vendorDashboard/vendorDashboard.module';
+import { UserDashboardModule } from './userDashboard/userDashboard.modules';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/dinamoDB'),
     VendorDashboardModule,
+    UserDashboardModule
   ],
   controllers: [],
   providers: [],
