baf4a27fbd8c0523ee61075dfb16f9a9f7dc9cce
magdybadr978 <magdibadr2002@gmail.com>
2024-10-01 15:09:26 +0300

    add atToCart service

--- src/userDashboard/cart/cart.service.ts
+++ src/userDashboard/cart/cart.service.ts
@@ -1,18 +1,42 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Types } from 'mongoose';
 import { CartRepository } from 'src/models/cart/cart.repository';
+import { ProductRepository } from 'src/models/product/product.repository';
+import { UserRepository } from 'src/models/user/user.repository';
+
 
 @Injectable()
 export class CartService {
-  constructor(private readonly cartRepository: CartRepository) {}
+  constructor(private readonly cartRepository: CartRepository ,private readonly userRepository : UserRepository , private readonly productRepositry : ProductRepository) {}
 
-  // Create a new cart for a user
-  async createCart(userId: Types.ObjectId, items: { product: Types.ObjectId; quantity: number }[]) {
-    const newCart = {
-      userId,
-      items,
-    };
-    return this.cartRepository.create(newCart);
+  // add to cart for a user
+  async addToCart(userId: Types.ObjectId, items: {productId: Types.ObjectId ,quantity: number}) {
+    // destruct data from items
+    const {productId , quantity } = items;
+    // check if user exist
+    const user = this.userRepository.getById(userId)
+    // if user not exist
+    if(!user) throw new NotFoundException("user not exist")
+      // check if product exist 
+    const product = this.productRepositry.getById(productId);
+    // check if product not exist
+    if(!product) throw new NotFoundException("product not found")
+      // get cart if exist
+    const cart = this.cartRepository.getById(userId)
+    // check if not exist
+    if(!cart){
+      await this.cartRepository.create({userId , items : []})
+    }
+    // if item already exist
+    const itemExist = (await cart).items.findIndex(item => item.productId.equals(productId))
+    if(itemExist > -1){
+      // add ++ on exist item
+      (await cart).items[itemExist].quantity += quantity;
+    }else {
+      (await cart).items.push({productId ,quantity })
+    }
+  
+    return this.cartRepository.update({userId},{items : (await cart).items},{new : true});
   }
 
   // Get the cart by user ID
@@ -25,7 +49,7 @@ export class CartService {
   }
 
   // Add or update items in the cart
-  async updateCart(userId: Types.ObjectId, items: { product: Types.ObjectId; quantity: number }[]) {
+  async updateCart(userId: Types.ObjectId, items: { productId: Types.ObjectId; quantity: number }[]) {
     return this.cartRepository.update({ userId }, { items }, { new: true, upsert: true });
   }
 
@@ -33,7 +57,7 @@ export class CartService {
   async removeItemFromCart(userId: Types.ObjectId, productId: Types.ObjectId) {
     const cart = await this.cartRepository.update(
       { userId },
-      { $pull: { items: { product: productId } } },
+      { $pull: { items: { productId: productId } } },
       { new: true },
     );
 
