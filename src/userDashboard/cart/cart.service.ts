9b2f3b97708fd3ffc6ec9049e06a9ff1a67d6f4a
magdybadr978 <magdibadr2002@gmail.com>
2024-10-02 02:21:18 +0300

    fix : some errors in addToCart

--- src/userDashboard/cart/cart.service.ts
+++ src/userDashboard/cart/cart.service.ts
@@ -1,16 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Types } from 'mongoose';
 import { CartRepository } from 'src/models/cart/cart.repository';
+import { CartDocument } from 'src/models/cart/cart.schema';
 import { ProductRepository } from 'src/models/product/product.repository';
 import { UserRepository } from 'src/models/user/user.repository';
 
-
 @Injectable()
 export class CartService {
-  constructor(private readonly cartRepository: CartRepository ,private readonly userRepository : UserRepository , private readonly productRepositry : ProductRepository) {}
+  constructor(
+    private readonly cartRepository: CartRepository,
+    private readonly userRepository: UserRepository,
+    private readonly productRepositry: ProductRepository,
+  ) {}
 
   // add to cart for a user
-  async addToCart(userId: Types.ObjectId, items: { productId: Types.ObjectId; quantity: number }) {
+  async addToCart(
+    userId: Types.ObjectId,
+    items: { productId: Types.ObjectId; quantity: number },
+  ) {
     // Destructure data from items
     const { productId, quantity } = items;
     // Check if user exists
@@ -18,17 +25,26 @@ export class CartService {
     if (!user) throw new NotFoundException('User does not exist');
     // Check if product exists
     const product = await this.productRepositry.getById(productId);
+
+    //  failed
     if (!product) throw new NotFoundException('Product not found');
     // Get cart if it exists
     let cart = await this.cartRepository.getOne({ userId });
+    console.log({ cart });
+
     // If the cart does not exist, create a new one
     if (!cart) {
-      cart = await this.cartRepository.create({userId , items :[]});
+      cart = (await this.cartRepository.create({
+        userId: userId,
+        items: [],
+      })) as CartDocument;
     }
-  
+
     // Check if the item already exists in the cart
-    const itemExistIndex = cart.items.findIndex(item => item.productId.equals(productId));
-  
+    const itemExistIndex = cart.items.findIndex((item) =>
+      item.productId.equals(productId),
+    );
+
     if (itemExistIndex > -1) {
       // If the item exists, increase the quantity
       cart.items[itemExistIndex].quantity += quantity;
@@ -36,28 +52,24 @@ export class CartService {
       // If the item doesn't exist, push it to the cart
       cart.items.push({ productId, quantity });
     }
-  
+
     // Update the cart with the new or updated items
-    return this.cartRepository.update({ _id: cart._id }, { items: cart.items }, { new: true });
+    return this.cartRepository.update(
+      { _id: cart._id },
+      { items: cart.items },
+      { new: true },
+    );
   }
-  
+
   // Get the cart by user ID
   async getCartByUserId(userId: Types.ObjectId) {
-    const cart = await this.cartRepository.getById(userId);
+    const cart = await this.cartRepository
+      .getOne({userId} , {},{ populate: [{ path: 'items.productId'}]})
+      
     if (!cart) {
       throw new NotFoundException('Cart not found');
     }
     return cart;
-  }
-
-  // Add or update items in the cart
-  async updateCart(userId: Types.ObjectId, items: { productId: Types.ObjectId; quantity: number }[]) {
-    // check if user cart exist by userId
-    const cart = await this.cartRepository.getById(userId)
-    //failed
-    if(!cart) throw new NotFoundException("cart not found")
-      // return response
-    return this.cartRepository.update({ userId }, { items }, { new: true });
   }
 
   // Remove an item from the cart
@@ -68,7 +80,7 @@ export class CartService {
       { $pull: { items: { productId: productId } } },
       { new: true },
     );
-   // failed
+    // failed
     if (!cart) {
       throw new NotFoundException('Cart not found');
     }
@@ -77,10 +89,12 @@ export class CartService {
   }
 
   // Delete a cart by user ID
-  async deleteCart(userId: string):Promise<{message : string}> {
+  async deleteCart(userId: string): Promise<{ message: string }> {
     // delete item by userId
-    const result = await this.cartRepository.delete({_id : new Types.ObjectId(userId)});
-    // failed 
+    const result = await this.cartRepository.delete({
+      userId: new Types.ObjectId(userId),
+    });
+    // failed
     if (result.deletedCount === 0) {
       throw new NotFoundException('Cart not found');
     }
