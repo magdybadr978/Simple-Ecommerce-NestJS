3885df2080d07cd5b0cb69d69ac2882c205fef88
magdybadr978 <magdibadr2002@gmail.com>
2024-10-02 02:22:22 +0300

    add UserMongoModule,     ProductModule,

--- src/userDashboard/cart/cart.modules.ts
+++ src/userDashboard/cart/cart.modules.ts
@@ -1,23 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Cart, cartSchema } from 'src/models/cart/cart.schema';
-import { CartController } from './cart.controller';
 import { CartRepository } from 'src/models/cart/cart.repository';
-import { CartService } from './cart.service';
-import { User, userSchema } from 'src/models/user/user.schema';
-import { UserRepository } from 'src/models/user/user.repository';
-import { UserService } from '../user/user.service';
-import { Person, personSchema } from 'src/models/common/person.schema';
-import { UserModule } from '../user/user.modules';
+import { Cart, cartSchema } from 'src/models/cart/cart.schema';
+import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
 import { ProductModule } from 'src/vendorDashboard/product/product.modules';
+import { CartController } from './cart.controller';
+import { CartService } from './cart.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Cart.name, schema: cartSchema }]),
-       UserModule,
-       ProductModule
-      //  { name: Person.name, schema: personSchema ,discriminators: [{name : User.name , schema : userSchema}] },
+    MongooseModule.forFeature([{ name: Cart.name, schema: cartSchema }]),
+    //  UserModule,
+    UserMongoModule,
+    ProductModule,
+    //  { name: Person.name, schema: personSchema ,discriminators: [{name : User.name , schema : userSchema}] },
   ],
   controllers: [CartController],
   providers: [CartService, CartRepository],
