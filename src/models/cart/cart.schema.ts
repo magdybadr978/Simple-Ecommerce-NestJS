4539bb7de8f6eb93df57bc2a12083cad8acdfea7
magdybadr978 <magdibadr2002@gmail.com>
2024-10-02 02:27:01 +0300

    add interface Items

--- src/models/cart/cart.schema.ts
+++ src/models/cart/cart.schema.ts
@@ -1,17 +1,28 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import mongoose, { Types , Document } from "mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Types, Document } from 'mongoose';
 
 export type CartDocument = Cart & Document;
 
-@Schema({timestamps : true})
-export class Cart{
-  @Prop({type : mongoose.Schema.Types.ObjectId , ref : "User" , required : true})
-  userId : Types.ObjectId;
-
-  @Prop([{
-    productId : {type : mongoose.Schema.Types.ObjectId , ref : "Product" ,required : true},
-    quantity : { type : Number , required : true, default : 1}
-  }])
-  items : { productId : Types.ObjectId , quantity : number}[]
+interface Items {
+  productId: Types.ObjectId;
+  quantity: number;
 }
-export const  cartSchema = SchemaFactory.createForClass(Cart);
+
+@Schema({ timestamps: true })
+export class Cart {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  userId: Types.ObjectId;
+
+  @Prop([
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: { type: Number, required: true, default: 1 },
+    },
+  ])
+  items: Items[];
+}
+export const cartSchema = SchemaFactory.createForClass(Cart);
