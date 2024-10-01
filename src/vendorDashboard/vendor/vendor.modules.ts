346d925633a40283200f0ca28d318bfe2bd4eb8a
magdybadr978 <magdibadr2002@gmail.com>
2024-10-02 02:17:53 +0300

    add UserMongoModule

--- src/vendorDashboard/vendor/vendor.modules.ts
+++ src/vendorDashboard/vendor/vendor.modules.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Person, personSchema } from 'src/models/common/person.schema';
-import { Vendor, vendorSchema } from 'src/models/vendor/vendor.schema';
-import { VendorController } from './vendor.controller';
-import { VendorRepository } from 'src/models/vendor/vendor.repository';
-import { VendorService } from './vendor.service';
 import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
+import { VendorController } from './vendor.controller';
+import { VendorService } from './vendor.service';
 
 @Module({
   imports: [
