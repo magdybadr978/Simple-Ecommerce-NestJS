fc5b7e8bcd935171331356025f23c615ef05b019
magdybadr978 <magdibadr2002@gmail.com>
2024-10-02 02:17:28 +0300

    fix : add UserMongoModule

--- src/vendorDashboard/vendor/vendor.modules.ts
+++ src/vendorDashboard/vendor/vendor.modules.ts
@@ -1,20 +1,19 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { Person, personSchema } from "src/models/common/person.schema";
-import { Vendor, vendorSchema } from "src/models/vendor/vendor.schema";
-import { VendorController } from "./vendor.controller";
-import { VendorRepository } from "src/models/vendor/vendor.repository";
-import { VendorService } from "./vendor.service";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Person, personSchema } from 'src/models/common/person.schema';
+import { Vendor, vendorSchema } from 'src/models/vendor/vendor.schema';
+import { VendorController } from './vendor.controller';
+import { VendorRepository } from 'src/models/vendor/vendor.repository';
+import { VendorService } from './vendor.service';
+import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
 
 @Module({
-  imports: [ MongooseModule.forFeature([
-    {
-      name : Person.name,
-      schema : personSchema,
-      discriminators : [{ name : Vendor.name , schema : vendorSchema}]
-    }])],
-    controllers : [VendorController],
-    providers : [VendorRepository, VendorService],
-    exports : [VendorRepository,VendorService],
+  imports: [
+    
+    UserMongoModule,
+  ],
+  controllers: [VendorController],
+  providers: [VendorService],
+  exports: [VendorService],
 })
 export class VendorModule {}
