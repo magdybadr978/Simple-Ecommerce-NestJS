3dfddd40e647c761a8d9dce0de7760dd0291d302
magdybadr978 <magdibadr2002@gmail.com>
2024-10-02 03:10:26 +0300

    refactor : use response dto to handel response

--- src/userDashboard/user/user.service.ts
+++ src/userDashboard/user/user.service.ts
@@ -1,27 +1,32 @@
 
 
 import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import * as bcrypt from "bcryptjs";
 import { Types } from "mongoose";
+import { CreateResponse, DeleteResponse, GetAllResponse, GetOneResponse, UpdateResponse } from "src/common/dto/response.dto";
 import { UserRepository } from "src/models/user/user.repository";
-import * as bcrypt from "bcryptjs"
+import { User, UserDocument } from "src/models/user/user.schema";
+import { CreateUserDTO, UpdateUserDTO } from "./dto";
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository : UserRepository) {}
 
 
     // signUp for user
-    async signUp(createUserDTO : {name : string , phone : string , password : string}){
+    async signUp(createUserDTO : CreateUserDTO): Promise<CreateResponse<User>>{
       // check if user exist
-      const userExist = await this.userRepository.getOne({ phone : createUserDTO.phone})
+      const userExist = await this.userRepository.getOne({ phone : createUserDTO.phone}) 
       // if exist throw error
       if(userExist) throw new BadRequestException("user already exist")
       // Hash password
       const hashPassword = await bcrypt.hash(createUserDTO.password,10)
+      // make variable to add user 
+      const user = this.userRepository.create({...createUserDTO , password : hashPassword})as unknown as  UserDocument
       // send response
-      return this.userRepository.create({...createUserDTO , password : hashPassword})
+      return {success : true , data : user}
     }
      // signIn for user
-    async signIn(signInDTO : {phone : string , password : string}){
+    async signIn(signInDTO : CreateUserDTO): Promise<CreateResponse<User>>{
       // check if user exist
       const user = await this.userRepository.getOne({ phone : signInDTO.phone});
       // if user not found
@@ -31,29 +36,30 @@ export class UserService {
       // check valid password 
       if (!passwordValid) throw new UnauthorizedException ('Invalid credentials');
       // return user details 
-      return { user };
+      return { success : true , data : user };
     }
   
     // Get all Users
-    async getAllUsers() {
-      return await this.userRepository.getAll({});
+    async getAllUsers(): Promise<GetAllResponse<User>> {
+      // use method getAll
+      const users = await this.userRepository.getAll({});
+      // failed
+      if(users.length == 0) throw new NotFoundException("not found users")
+      return {success : true , data : users}
     }
   
     // Get a User by id
-    async getUserById(id: string){
+    async getUserById(id: string):Promise<GetOneResponse<User>>{
       // find User 
       const user = await this.userRepository.getOne({ _id: new Types.ObjectId(id) });
       // check id User exist
       if(!user) throw new NotFoundException("Not found User");
       // return response
-      return {user}
+      return { success : true , data : user}
     }
   
     // Update User data by Id
-    async updateUser(
-      id : string,
-      updateUserDTO : {name: string,phone: string,password: string}
-    ) {
+    async updateUser(id : string,updateUserDTO : UpdateUserDTO):Promise<UpdateResponse<User>> {
       // find user
       const user = await this.userRepository.getOne({_id : new Types.ObjectId(id)})
       //if not found user
@@ -62,18 +68,20 @@ export class UserService {
       const hashPassword = await bcrypt.hash(updateUserDTO.password,8);
       // wrapping data in update variable
       const update = { ...updateUserDTO , password :hashPassword}
+      // create variable to add updated data
+      const updatedUser = this.userRepository.update({ _id: new Types.ObjectId(id) },update,{ new: true }) as unknown as  UserDocument;
       // return response  
-      return this.userRepository.update({ _id: new Types.ObjectId(id) },update,{ new: true });
+      return { success : true , data : updatedUser}
         
     }
   
     // Delete User data by Id
-    async deleteUser(id: string): Promise<{message : string}> {
+    async deleteUser(id: string): Promise<DeleteResponse> {
       // use method delete 
       const user = await this.userRepository.delete({ _id: new Types.ObjectId(id) });
       // check if user deleted or not
       if(user.deletedCount == 0) throw new NotFoundException("user not found")
       // send response
-      return {message : "Deleted user successfully"}
+      return {success : true}
     }
 }
