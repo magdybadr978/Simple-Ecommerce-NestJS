f87c8807b296002456f6abb1c812d3b7d42fcd48
magdybadr978 <magdibadr2002@gmail.com>
2024-10-05 22:25:14 +0300

    add CreateVendorDTO

--- src/vendorDashboard/vendor/dto/index.ts
+++ src/vendorDashboard/vendor/dto/index.ts
@@ -1,10 +1,29 @@
 import { PartialType } from "@nestjs/swagger";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsPhoneNumber, IsString, IsStrongPassword } from "class-validator";
 
-export class CreateVendorDTO{
-      name: string;
-      phone: string;
-      password: string;   
+
+export class CreateVendorDTO {
+  @ApiProperty()
+  @IsString()
+  name : string;
+
+  @ApiProperty()
+  @IsPhoneNumber('EG')   // allow only egyptian phone numbers
+  phone : string;
+
+  @ApiProperty()
+  @IsStrongPassword({
+    minLength : 8,
+    minLowercase : 1,
+    minNumbers : 1,
+    minSymbols : 1,
+    minUppercase : 1
+  })
+  password : string
 }
+
+
 
 export class UpdateVendorDTO extends PartialType(CreateVendorDTO){}
 
