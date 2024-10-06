a961940af603f1d6e9fb8bcd987a972a1cb4c007
magdybadr978 <magdibadr2002@gmail.com>
2024-10-06 13:46:06 +0300

    use .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VendorDashboardModule } from './vendorDashboard/vendorDashboard.module';
 import { UserDashboardModule } from './userDashboard/userDashboard.modules';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/dinamoDB'),
+    ConfigModule.forRoot({ isGlobal : true ,envFilePath : './src/config/.env'}),
+    MongooseModule.forRoot(process.env.DB_URL),
     VendorDashboardModule,
     UserDashboardModule
   ],
