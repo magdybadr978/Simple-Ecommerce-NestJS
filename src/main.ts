f5fad5e1cb8fabe850324bbbf7d1eace9aa1f80d
magdybadr978 <magdibadr2002@gmail.com>
2024-10-08 12:28:59 +0300

    useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { RolesGuard } from './Guards/Authorization';
 const port = process.env.PORT || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  //app.useGlobalGuards(new RolesGuard())
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(port);
 }
 bootstrap();
