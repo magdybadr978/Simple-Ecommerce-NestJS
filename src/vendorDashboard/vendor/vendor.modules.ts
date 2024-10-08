6539a28517fbbe45e1d5049fc2fcf1997f3402c0
magdybadr978 <magdibadr2002@gmail.com>
2024-10-08 15:50:18 +0300

    add RolesGuard

--- src/vendorDashboard/vendor/vendor.modules.ts
+++ src/vendorDashboard/vendor/vendor.modules.ts
@@ -4,14 +4,14 @@ import { AuthService } from 'src/Guards/Auth.service';
 import { UserMongoModule } from 'src/shared/modules/user-mongo.module';
 import { VendorController } from './vendor.controller';
 import { VendorService } from './vendor.service';
+import { RolesGuard } from 'src/Guards/Authorization';
 
 @Module({
   imports: [
-    
     UserMongoModule,
   ],
   controllers: [VendorController],
-  providers: [VendorService,AuthService,JwtService],
+  providers: [VendorService,AuthService,JwtService,RolesGuard],
   exports: [VendorService],
 })
 export class VendorModule {}
