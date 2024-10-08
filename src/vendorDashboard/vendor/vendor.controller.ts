3e325194fbd96180aaa6e804c296644d64a7f18a
magdybadr978 <magdibadr2002@gmail.com>
2024-10-08 15:49:56 +0300

    add RolesGuard

--- src/vendorDashboard/vendor/vendor.controller.ts
+++ src/vendorDashboard/vendor/vendor.controller.ts
@@ -1,25 +1,35 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { VendorService } from "./vendor.service";
-import { CreateVendorDTO, UpdateVendorDTO } from "./dto";
+import { CreateVendorDTO, SignInDTO, UpdateVendorDTO } from "./dto";
 import { AuthGuard } from "src/Guards/Authentication";
+import { RolesGuard } from "src/Guards/Authorization";
+import { Roles } from "src/common/Guards/roles.decorator";
+import { request } from "http";
 
 
 @Controller('dashboard-vendor/vendor')
 export class VendorController{
   constructor(private readonly vendorService :VendorService) {}
 
-  @Post()
-  @UseGuards(AuthGuard)
+  @Post('/signUp')
   @UsePipes(ValidationPipe)
   async createVendor(
     @Body()
     createVendorDTO: CreateVendorDTO
   ) {
-    return await this.vendorService.createVendor(createVendorDTO);
+    return await this.vendorService.signup(createVendorDTO);
   }
 
+  @Post('/signIn')
+  @UsePipes(ValidationPipe)
+  async signIn(@Body() signInDTO : SignInDTO){
+    return await this.vendorService.signIn(signInDTO);
+  }
+
+
   @Get()
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard,RolesGuard)
+  @Roles('Vendor')
   async getAllVendors() {
     return await this.vendorService.getAllVendors();
   }
@@ -31,22 +41,25 @@ export class VendorController{
   }
 
   @Put(':id')
-  @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard,RolesGuard)
+  @Roles('Vendor')
   @UsePipes(ValidationPipe)
   async updateVendor(
     @Param('id') id: string,
     @Body()
-    updateVendorDTO: UpdateVendorDTO
+    updateVendorDTO: UpdateVendorDTO,@Req() request : any
   ) {
     return this.vendorService.updatevendor(
       id,
-      updateVendorDTO
+      updateVendorDTO,
+      request
     );
   }
 
   @Delete(':id')
-  @UseGuards(AuthGuard)
-  async deleteVendor(@Param('id') id: string) {
-    return await this.vendorService.deleteVendor(id);
+  @UseGuards(AuthGuard,RolesGuard)
+  @Roles('Vendor')
+  async deleteVendor(@Param('id') id: string , @Req() request : any)  {
+    return await this.vendorService.deleteVendor(id, request);
   }
 }
