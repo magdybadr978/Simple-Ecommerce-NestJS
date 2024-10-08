209fb65d3843d02fcf93335f29ee9dcdfe14b304
magdybadr978 <magdibadr2002@gmail.com>
2024-10-08 12:30:10 +0300

    add RolesGuard

--- src/userDashboard/user/user.modules.ts
+++ src/userDashboard/user/user.modules.ts
@@ -4,13 +4,14 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { AuthService } from 'src/Guards/Auth.service';
 import { JwtService } from '@nestjs/jwt';
+import { RolesGuard } from 'src/Guards/Authorization';
 
 @Module({
   imports: [
     UserMongoModule,
   ],
   controllers: [UserController],
-  providers: [UserService,AuthService,JwtService],
+  providers: [UserService,AuthService,JwtService,RolesGuard],
   exports: [UserService],
 })
 export class UserModule {}
