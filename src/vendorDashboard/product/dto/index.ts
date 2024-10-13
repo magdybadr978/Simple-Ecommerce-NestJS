1ac8ef03b305b10d36cdecbd42b436e252a2590b
magdybadr978 <magdibadr2002@gmail.com>
2024-10-13 14:02:52 +0300

    add prodcutDTO for order

--- src/vendorDashboard/product/dto/index.ts
+++ src/vendorDashboard/product/dto/index.ts
@@ -21,4 +21,21 @@ export class CreateProductDTO {
   vendorId: Types.ObjectId;
 }
 
+
+// for order
+export class ProductDTO {
+  @ApiProperty()
+  @IsMongoId()
+  productId: Types.ObjectId; // Instead of vendorId, use productId for the order
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsPositive()
+  price: number;
+}
+
 export class UpdateProductDTO extends PartialType(CreateProductDTO) {}
