48a220735e2650251bab1c48b7aaacdbc0531801
magdybadr978 <magdibadr2002@gmail.com>
2024-10-13 14:00:13 +0300

    add : controller

---
+++ src/userDashboard/order/order.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Param, Post } from "@nestjs/common";
+import { OrderService } from "./order.service";
+import { CreateOrderDTO } from "./dto";
+import { Types } from "mongoose";
+
+
+@Controller('dashboard-user/order')
+export class OrderController{
+  constructor(private readonly orderService : OrderService) {}
+
+  @Post('create')
+  async createOrder(@Body() createOrderDTO : CreateOrderDTO){
+    return await this.orderService.createOrder(createOrderDTO)
+  }
+
+  @Post('/:id')
+  async cancelOrder(@Param() id : string){
+    return await this.orderService.cancelOrder(new Types.ObjectId(id))
+  }
+
+
+
+}
