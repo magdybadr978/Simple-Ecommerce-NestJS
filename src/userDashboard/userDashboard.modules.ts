ca817f3e75694779975975a251eb868899c11b33
magdybadr978 <magdibadr2002@gmail.com>
2024-10-13 13:59:21 +0300

    add orderModule

--- src/userDashboard/userDashboard.modules.ts
+++ src/userDashboard/userDashboard.modules.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.modules';
 import { CartModule } from './cart/cart.modules';
+import { OrderModule } from './order/order.modules';
 
 
 @Module({
-  imports: [UserModule,CartModule],
+  imports: [UserModule,CartModule,OrderModule],
   controllers: [],
   providers: [],
 })
