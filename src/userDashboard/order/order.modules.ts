987ed4f3d5c62960e38278f6b1219263860590cb
magdybadr978 <magdibadr2002@gmail.com>
2024-10-21 14:20:36 +0300

    fix : schema

--- src/userDashboard/order/order.modules.ts
+++ src/userDashboard/order/order.modules.ts
@@ -1,15 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
-import { cartSchema } from "src/models/cart/cart.schema";
-import { Order } from "src/models/order/order.schema";
-import { OrderController } from "./order.controller";
-import { OrderService } from "./order.service";
 import { OrderRepository } from "src/models/order/order.repository";
+import { Order, orderSchema } from "src/models/order/order.schema";
 import { UserMongoModule } from "src/shared/modules/user-mongo.module";
 import { ProductModule } from "src/vendorDashboard/product/product.modules";
+import { OrderController } from "./order.controller";
+import { OrderService } from "./order.service";
 
 @Module({
-  imports : [ MongooseModule.forFeature([{ name : Order.name , schema : cartSchema}]),
+  imports : [ MongooseModule.forFeature([{ name : Order.name , schema : orderSchema}]),
   UserMongoModule,
   ProductModule
 ],
