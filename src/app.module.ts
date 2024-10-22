27b2c732c55e57aadc88eb3842554474f9e39ef3
magdybadr978 <magdibadr2002@gmail.com>
2024-10-22 21:54:21 +0300

    add dirname

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import { UserDashboardModule } from './userDashboard/userDashboard.modules';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './Guards/Authorization';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal : true ,envFilePath : './src/config/.env'}),
     MongooseModule.forRoot(process.env.DB_URL),
+    ServeStaticModule.forRoot({ rootPath : join(__dirname , ".." , 'uploads')}),
     VendorDashboardModule,
     UserDashboardModule
   ],
