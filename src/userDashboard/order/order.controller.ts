a331b41d6db1a91d17ca2b71b31f408b34f3bdd9
magdybadr978 <magdibadr2002@gmail.com>
2024-10-23 15:07:28 +0300

    add get order controller

--- src/userDashboard/order/order.controller.ts
+++ src/userDashboard/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { OrderService } from "./order.service";
 import { CreateOrderDTO } from "./dto";
 import { Types } from "mongoose";
@@ -16,6 +16,11 @@ export class OrderController{
   @Post('/:id')
   async cancelOrder(@Param() id : string){
     return await this.orderService.cancelOrder(new Types.ObjectId(id))
+  }
+
+  @Get('/:id')
+  async getOrder(@Param() id : string){
+    return await this.orderService.getOrder(new Types.ObjectId(id))
   }
 
 
