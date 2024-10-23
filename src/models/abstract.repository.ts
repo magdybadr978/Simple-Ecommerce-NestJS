0db97a8105810499a9bddf3c5b7a5d008864aa30
magdybadr978 <magdibadr2002@gmail.com>
2024-10-23 14:49:41 +0300

    add buildQuery and APIFeatures

--- src/models/abstract.repository.ts
+++ src/models/abstract.repository.ts
@@ -1,3 +1,4 @@
+import { isArray, isEmpty, isNil } from 'lodash';
 import {
   Document,
   FilterQuery,
@@ -6,6 +7,7 @@ import {
   QueryOptions,
   Types,
 } from 'mongoose';
+import { GetAll } from 'src/common/type';
 
 export abstract class AbstractRepository<T> {
   private repo: Model<T & Document>;
@@ -23,12 +25,87 @@ export abstract class AbstractRepository<T> {
     return newDocument.save();
   }
 
+  // public async getAll(
+  //   query: FilterQuery<T>,
+  //   params?: ProjectionType<T>,
+  //   options?: QueryOptions<T>,
+  // ) {
+  //   return this.nModel.find(query, params, options).lean().exec();
+  // }
+
+  private _buildQuery(func, params) {
+    if (isNil(params)) return func;
+
+    const { fields, lean, populate } = params;
+
+    if (!isNil(fields)) func.select(fields);
+    if (!isNil(lean)) func.lean();
+
+    if (populate) {
+      for (let i = 0; i < populate.length; i += 1) {
+        if (isArray(populate[i])) {
+          func.populate(...populate[i]); 
+        } else {
+          func.populate(populate[i]);
+        }
+      }
+    }
+
+    return func;
+  }
+
   public async getAll(
-    query: FilterQuery<T>,
-    params?: ProjectionType<T>,
-    options?: QueryOptions<T>,
+    query: any,
+    params: GetAll = { limit: 25, page: 1, paginate: true },
   ) {
-    return this.nModel.find(query, params, options).lean().exec();
+    const {
+      limit = 25,
+      page = 1,
+      paginate = true,
+      sort,
+      order,
+      search,
+    } = params || {};
+    const skip = (page - 1) * limit;
+    let nResult = this._buildQuery(this.repo.find(query || {}), params);
+
+    if (search) {
+      nResult = nResult.where({
+        $or: [
+          { name: { $regex: new RegExp(search, 'i') } },
+          { 'translation.name': { $regex: new RegExp(search, 'i') } },
+          { title: { $regex: new RegExp(search, 'i') } },
+          { 'translation.title': { $regex: new RegExp(search, 'i') } },
+        ],
+      });
+    }
+
+    if (paginate) {
+      nResult.skip(skip);
+      nResult.limit(limit);
+    }
+
+    if (sort) {
+      nResult.sort({ [sort]: order });
+    }
+
+    let result = await nResult.exec();
+
+    if (paginate) {
+      const count = isEmpty(query)
+        ? await this.model.collection.countDocuments({})
+        : await this.model.countDocuments(query);
+
+      const pages = Math.ceil(count / limit) || 1;
+      result = {
+        data: result,
+        currentPage: skip === 0 ? 1 : Math.ceil(skip / limit) + 1,
+        numberOfPages: pages,
+        numberOfRecords: count,
+      };
+    }
+
+    return result;
   }
 
   public getOne(
